584b28ef45942c3cefecabcbc017f20e980fb9f5
muhammed k <muhammed.kus@agteks.com>
2020-09-22 10:44:40 +0300

    add button

--- github_test/Form1.cs
+++ github_test/Form1.cs
@@ -21,5 +21,10 @@ namespace github_test
         {
             int a = 4;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("message");
+        }
     }
 }
